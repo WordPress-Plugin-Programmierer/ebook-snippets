d2cc9b4fa1dfa52999b59433511aff581437d9fa
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-10-27 14:14:47 +0100

    New snippet 2.1

---
+++ 2.1.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Plugin Name: Mein erstes Plugin
+	Plugin URI: http://beispiel.de/plugins/mein-plugin
+	Description: Mein erstes Plugin
+	Version: 1.0
+	Author: Max Mustermann
+	Author URI: http://beispiel.de
+	License: GPLv2
+*/
