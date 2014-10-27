d4de37d2368c7b4df95296b5f2f8061caab54eda
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-10-27 16:23:52 +0100

    Added 2.6.3 snippet

---
+++ 2.6.3.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: PHPDoc für Klassen
+*/
+
+class Meine_Klasse {
+
+	/**
+	 * Prüft ob Motor gerade läuft.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @var bool $running
+	 */
+	private $running = false;
+}
