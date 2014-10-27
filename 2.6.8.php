d28e6da2bd1eac6c129b3acad854761b93cedc94
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-10-27 16:25:26 +0100

    Added 2.6.8 snippet

---
+++ 2.6.8.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: PHPDoc für Klassenkonstanten
+*/
+
+class Meine_Klasse {
+	/**
+	 * Wert für den Start.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var int
+	 */
+	const START = 0;
+}
