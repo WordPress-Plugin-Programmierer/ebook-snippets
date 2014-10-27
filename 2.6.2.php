d3dbf4475c128c443d04ff60b1b26276de0f26f1
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-10-27 16:22:23 +0100

    Added 2.6.2 snippet

---
+++ 2.6.2.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: PHPDoc für Funktionen und Methoden
+*/
+
+/**
+ * Kurzbeschreibung.
+ *
+ * Lange Beschreibung.
+ *
+ * @since      x.x.x
+ * @deprecated x.x.x Benutze stattdessen neuer_funktionsname().
+ * @access     private (für Methoden nur wenn als privat markiert)
+ *
+ * @see        Function/method/class relied on
+ * @global Typ $var Kurzbeschreibung.
+ *
+ * @param  Typ $var Beschreibung
+ * @param  Typ $var Optional. Beschreibung.
+ *
+ * @return Typ Beschreibung.
+ */
