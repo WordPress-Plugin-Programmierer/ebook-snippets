6403756b757e773b778b30f066e418296d502c2b
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-10-27 14:24:37 +0100

    Added 2.5.2 snippet

---
+++ 2.5.2.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Einrückung
+*/
+
+
+// Nicht empfohlen
+$mein_array = array(
+	'foo' => 'wert1',
+	'foo12' => 'wert2',
+	'foo34' => 'wert3',
+	'foo567' => 'wert4',
+);
+
+// Empfohlen:
+$mein_array = array(
+	'foo'    => 'wert1',
+	'foo12'  => 'wert2',
+	'foo34'  => 'wert3',
+	'foo567' => 'wert4',
+);
