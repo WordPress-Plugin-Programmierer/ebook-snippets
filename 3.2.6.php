89cc34afa0656739afbfb60d01ff72dd1389a758
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-11-12 09:12:47 +0100

    Added 3.2.6 snippet

---
+++ 3.2.6.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Remove Actions
+*/
+
+
+remove_action( 'wp_head', 'wp_generator', 10 );
+add_action( 'wp_head', 'wp_generator', 0 );
+
+$is_added = has_action( 'wp_head', 'wp_generator' );
+
+if ( $is_added !== false ) {
+	# Funktion wurde gefunden
+}
