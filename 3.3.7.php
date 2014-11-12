a95f05bf767db248f1809d267b80245e4c1d0484
code-flow <floriansimeth@Florians-Mini.fritz.box>
2014-11-12 10:27:11 +0100

    Added 3.3.7 snippet

---
+++ 3.3.7.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: current_filter() Funktion
+*/
+
+add_filter( 'the_content', 'mm_filter_content' );
+add_filter( 'the_excerpt', 'mm_filter_content' );
+
+function mm_filter_content( $content ) {
+	if ( 'the_content' == current_filter() ) {
+	// Mache etwas um den Inhalt zu veärndern
+		return $content;
+	}
+
+	// Mache etwas anderes um den Excerpt zu verändern
+	return $content;
+}
