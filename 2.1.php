23131bceb173e125f64ffcb2571cced79045811e
Florian Simeth <florian.simeth@online.de>
2015-01-03 12:37:02 +0100

    Updated description to match screenshot

--- 2.1.php
+++ 2.1.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Mein erstes Plugin
 	Plugin URI: http://beispiel.de/plugins/mein-plugin
-	Description: Mein erstes Plugin
+	Description: Diest ist mein erstes Plugin
 	Version: 1.0
 	Author: Max Mustermann
 	Author URI: http://beispiel.de
