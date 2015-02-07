fd1ab1d2fbae67358c32031b88378ef6a28565fd
Florian Simeth <florian@simeth.me>
2015-02-07 10:40:48 +0100

    Added 13.7. snippet

---
+++ 13.7.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Überprüfung auf Eltern-Term
+*/
+
+// Ist ID 18 (Krimi) ein Kindelement von 20 (Belletristik)?
+var_dump( term_is_ancestor_of( 20, 18, 'category' ) ); // gibt true
+
+Ist ID 20 (Belletristik) ein Kindelement von ID 18 (Krimi)
+var_dump( term_is_ancestor_of( 18, 20, 'category' ) ); // gibt false
+
+Ist ID 21 (Klassiker) ein Kindelement von ID 20 (Belletristik)?
+var_dump( term_is_ancestor_of( 20, 21, 'category' ) ); // gibt true
