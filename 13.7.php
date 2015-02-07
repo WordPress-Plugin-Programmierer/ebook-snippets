3015a7c0accbd00a8dcd606cf9df6d93d654fdd4
Florian Simeth <florian@simeth.me>
2015-02-07 10:42:47 +0100

    Added comments the right way ;)

--- 13.7.php
+++ 13.7.php
@@ -6,8 +6,8 @@ Plugin Name: Überprüfung auf Eltern-Term
 // Ist ID 18 (Krimi) ein Kindelement von 20 (Belletristik)?
 var_dump( term_is_ancestor_of( 20, 18, 'category' ) ); // gibt true
 
-Ist ID 20 (Belletristik) ein Kindelement von ID 18 (Krimi)
+// Ist ID 20 (Belletristik) ein Kindelement von ID 18 (Krimi)
 var_dump( term_is_ancestor_of( 18, 20, 'category' ) ); // gibt false
 
-Ist ID 21 (Klassiker) ein Kindelement von ID 20 (Belletristik)?
+// Ist ID 21 (Klassiker) ein Kindelement von ID 20 (Belletristik)?
 var_dump( term_is_ancestor_of( 20, 21, 'category' ) ); // gibt true
