d48d1ffa390c384ac8c37cc0e18d70b99e55ef54
Florian Simeth <florian@simeth.me>
2015-02-24 15:52:22 +0100

    Added 5.3.5b snippet

---
+++ 5.3.5b.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Rolle des Benutzers ersetzen
+*/
+
+register_activation_hook( __FILE__, 'mm_add_capability' );
+
+function mm_add_capability() {
+	$new_role = add_role(
+		'configurator',
+		'Konfigurator',
+		array(
+			'read'           => true,
+			'manage_options' => true
+		)
+	);
+
+	if ( is_a( $new_role, 'WP_Role' ) ) {
+
+		$user = get_user_by( 'id', 2 );
+
+		if ( is_a( $user, 'WP_User' ) ) {
+			$user->set_role( 'configurator' );
+		}
+	}
+
+}
