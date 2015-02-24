b224d8df8f507fe3ad91d91a7be25aab07360675
Florian Simeth <florian@simeth.me>
2015-02-24 14:54:07 +0100

    Added 4.5.7 snippet

---
+++ 4.5.7.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Plugin Name: Vollständiges Widget-Beispiel
+*/
+
+class MM_Mein_Widget extends WP_Widget {
+	public function __construct() {
+		parent::__construct(
+			'mm_mein_widget',
+			'Mein Widget',
+			array( 'classname' => 'mm_mein_widget_class', 'description' => 'Macht etwas tolles' ),
+			array( 'width' => 500 )
+		);
+	}
+
+	public function form( $settings ) {
+
+		$field_1 = isset( $settings['field_1'] ) ? $settings['field_1'] : 'Noch nichts gespeichert';
+
+		echo '<p>';
+		echo '<label for="' . $this->get_field_id( 'field_1' ) . '">Feld 1</label>';
+		echo '<input id="' . $this->get_field_id( 'field_1' ) . '" class="widefat" name="' . $this->get_field_name( 'field_1' ) . '" type="text" value="' . esc_attr( $field_1 ) . '" />';
+		echo '</p>';
+
+	}
+
+	public function update( $new_settings, $old_settings ) {
+
+		// Nur Zahlen sind erlaubt
+		$new_settings['field_1'] = intval( $new_settings['field_1'] );
+
+		$new_settings['field_1'] = apply_filters( 'mm_mein_widget_field_1', $new_settings['field_1'] );
+
+		return $new_settings;
+	}
+
+	public function widget( $args, $settings ) {
+		echo $args['before_widget'];
+
+		echo $args['before_title'] . 'Mein Widget' . $args['after_title'];
+
+		echo 'Feld 1 enthält den Wert: ' . $settings['field_1'];
+
+		echo $args['after_widget'];
+	}
+
+}
+
+
+add_action( 'widgets_init', 'mm_register_mein_widget' );
+
+function mm_register_mein_widget() {
+	register_widget( 'MM_Mein_Widget' );
+}
+
+add_action( 'widgets_init', 'mm_register_mein_widget' );
+
+function mm_register_mein_widget() {
+	register_widget( 'MM_Mein_Widget' );
+}
