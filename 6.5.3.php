c9ebf86dd5883a6e03942853d2833f5fcfc8ac6d
Florian Simeth <florian@simeth.me>
2015-03-03 15:17:12 +0100

    Added 6.5.3 snippet

---
+++ 6.5.3.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: ABC-Plugin
+Plugin URI: http://my-website.com/abc-plugin/
+Description: This plugin displays ABC.
+Author: Max Mustermann
+Author URI: http://my-website.com/
+Version: 1.0.0
+Text Domain: abc
+Domain Path: /langs/
+*/
+
+load_plugin_textdomain( 'abc', false, dirname( plugin_basename( __FILE__ ) ) . '/langs/' );
+
+
+add_action( 'admin_menu', 'mm_admin_menu' );
+
+function mm_admin_menu() {
+	add_menu_page(
+		__( 'My Title', 'abc' ),
+		__( 'My Menu', 'abc' ),
+		'manage_options',
+		'mm-meins',
+		'mm_main_page_render',
+		plugins_url( 'pacman.png', __FILE__ )
+	);
+
+	add_submenu_page(
+		'mm-meins',
+		__( 'My Subheading Title', 'abc' ),
+		__( 'Submenu', 'abc' ),
+		'manage_options',
+		'mm-meins-unter-1',
+		'mm_sub1_page_render'
+	);
+}
+
+function mm_main_page_render() {
+	?>
+	<h1><?php _e( 'My Title', 'abc' ); ?></h1>
+<?php
+}
+
+function mm_sub1_page_render() {
+	echo '<h1>' . __( 'My Subheading Title', 'abc' ) . '</h1>';
+}
