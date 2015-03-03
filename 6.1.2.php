d6afd48f2f0fc9ac72681a3caf76cb40a9b292e6
Florian Simeth <florian@simeth.me>
2015-03-03 14:48:17 +0100

    Added 6.1.2 snippet

---
+++ 6.1.2.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:	Car Listing
+Plugin URI:	    http://my-website.com/car-listing-plugin/
+Description:	This plugin lists cars on a page.
+Author:		    Max Mustermann
+Author URI:	    http://my-website.com/
+Version:		1.0.0
+Text Domain:	mm_trans
+Domain Path:	/langs/
+*/
