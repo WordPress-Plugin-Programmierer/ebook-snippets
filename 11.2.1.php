ad70f2e9fdd3a0b29aa3008f01d5c7ed864673d4
Florian Simeth <florian@florian-simeth.de>
2015-06-25 14:54:46 +0200

    Added 11.2.1 snippet

---
+++ 11.2.1.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Book Post Type mit Capability-Option
+*/
+
+
+add_action( 'init', 'mm_register_post_type' );
+
+function mm_register_post_type() {
+
+	$labels = array(
+		'name'               => 'Books',
+		'singular_name'      => 'Book',
+		'menu_name'          => 'Books',
+		'name_admin_bar'     => 'Book',
+		'add_new'            => 'Add New',
+		'add_new_item'       => 'Add New Book',
+		'new_item'           => 'New Book',
+		'edit_item'          => 'Edit Book',
+		'view_item'          => 'View Book',
+		'all_items'          => 'All Books',
+		'search_items'       => 'Search Books',
+		'parent_item_colon'  => 'Parent Books:',
+		'not_found'          => 'No books found.',
+		'not_found_in_trash' => 'No books found in Trash.'
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => 'book',
+		'rewrite'            => array( 'slug' => 'book' ),
+		'capability_type'    => 'book',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'book', $args );
+
+}
