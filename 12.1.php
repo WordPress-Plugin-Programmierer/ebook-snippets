7ab88b89c2b5f284b6aceebc03fd77d81dbaf8c3
Florian Simeth <florian@florian-simeth.de>
2015-06-25 15:30:56 +0200

    Added 12.1 snippet

---
+++ 12.1.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Eigene Taxonomie
+*/
+
+add_action( 'init', 'mm_register_taxonomy_and_post' );
+
+function mm_register_taxonomy_and_post() {
+
+	$tax_labels = array(
+		'name'              => 'Genres',
+		'singular_name'     => 'Genre',
+		'search_items'      => 'Search Genres',
+		'all_items'         => 'All Genres',
+		'parent_item'       => 'Parent Genre',
+		'parent_item_colon' => 'Parent Genre:',
+		'edit_item'         => 'Edit Genre',
+		'update_item'       => 'Update Genre',
+		'add_new_item'      => 'Add New Genre',
+		'new_item_name'     => 'New Genre Name',
+		'menu_name'         => 'Genre',
+	);
+
+	register_taxonomy( 'genre', 'book', array(
+		'labels' => $tax_labels
+	) );
+
+	$labels = array(
+		'name'               => 'Books',
+		'singular_name'      => 'Book',
+		'menu_name'          => 'Books',
+		'name_admin_bar'     => 'Book',
+		'add_new'            => 'Add New',
+		'add_new_item'       => 'Add New Book',
+		'new_item'           => 'New Book',
+		'edit_item'          => 'Edit Book',
+		'view_item'          => 'View Book',
+		'all_items'          => 'All Books',
+		'search_items'       => 'Search Books',
+		'parent_item_colon'  => 'Parent Books:',
+		'not_found'          => 'No books found.',
+		'not_found_in_trash' => 'No books found in Trash.'
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => 'book',
+		'rewrite'            => array( 'slug' => 'book' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+		// Hinzufügen des Taxonomies
+		'taxonomies'         => array( 'genre' ),
+	);
+
+	register_post_type( 'book', $args );
+
+}
+
+get_taxonomy();
+
