29ad44f901448f560b11ab88eaafc59679e6cfe4
Florian Simeth <florian@florian-simeth.de>
2018-03-27 16:00:42 +0700

    Added chapter 17.0 example plugin

---
+++ 17.0-track-external-links/inc/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace f\tel;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+register_activation_hook( __FILE__, '\f\tel\plugin_activation' );
+
+/**
+ * Plugin activation.
+ * Create a new database table for outgoing links if it does not already exist.
+ *
+ * @since 0.1.0
+ */
+function plugin_activation() {
+
+	if ( ! function_exists( 'dbDelta' ) ) {
+		require ABSPATH . 'wp-admin/includes/upgrade.php';
+	}
+
+	global $wpdb;
+
+	$charset_collate = $wpdb->get_charset_collate();
+
+	dbDelta( "CREATE TABLE `{$wpdb->prefix}outgoing_links` (
+		`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+		`title` varchar(100) DEFAULT NULL,
+		`slug` varchar(100) DEFAULT NULL,
+		`link` text,
+		PRIMARY KEY (`id`),
+		UNIQUE KEY `id` (`id`)
+	) {$charset_collate};" );
+}
+
+require __DIR__ . '/backend.php';
+new Backend();
