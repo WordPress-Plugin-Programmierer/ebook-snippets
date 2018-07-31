1c34fde7a0d54df4bc7203d19a3c1be413c2312b
Florian Simeth <florian@florian-simeth.de>
2018-07-31 09:28:04 +0200

    Added chapter 20.4 plugin example

---
+++ 20.4-rating-in-rest/rating-in-rest-extend-_links.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Rating in REST
+Description: Extend _links in comments.
+*/
+
+add_filter( 'rest_prepare_comment', 'mm_rest_comment', 10, 3 );
+
+/**
+ * Extend _links in REST-Response.
+ *
+ * @param \WP_REST_Response $response
+ * @param \WP_Comment       $comment
+ * @param \WP_REST_Request  $request
+ *
+ * @return \WP_REST_Response
+ */
+function mm_rest_comment( $response, $comment, $request ) {
+
+	$response->add_link(
+		'https://my-rating-plugin.com/rating',
+		rest_url( "/wp/v2/comments/{$comment->comment_ID}/rating" )
+	);
+
+	return $response;
+}
+
+
+add_filter( 'rest_response_link_curies', 'mm_rest_curies' );
+
+/**
+ * Extend curies parameter in _links array.
+ *
+ * @param array $curies
+ *
+ * @return array
+ */
+function mm_rest_curies( $curies ) {
+
+	$curies[] = array(
+		'name'      => 'rating_plugin',
+		'href'      => 'https://my-rating-plugin.com/{rel}',
+		'templated' => true,
+	);
+
+	return $curies;
+}
