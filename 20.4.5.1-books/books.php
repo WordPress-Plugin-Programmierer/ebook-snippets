0e2393b40eda7d5f5cf24d98345f8d5f25ecf76f
Florian Simeth <florian@florian-simeth.de>
2018-08-29 15:42:28 +0200

    Added 20.4.5 example 1

---
+++ 20.4.5.1-books/books.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Custom Books
+Description: Example of how a custom post type is used in the REST API.
+*/
+
+# Register custom post types
+include __DIR__ . '/inc/types.php';
